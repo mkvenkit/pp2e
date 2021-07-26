75ea44cc6a3dfcdabe87b1ccd5567341ad56db7e
Mahesh Venkitachalam <mahesh.dgi@electronut.in>
2021-07-26 10:30:15 +0530

    updated

---
+++ simplegl/simpleglfw.py
@@ -0,0 +1,270 @@
+"""
+simpleglfw.py
+
+Simple Python OpenGL program that uses PyOpenGL + GLFW to get an 
+OpenGL 3.2 context. 
+
+Author: Mahesh Venkitachalam
+"""
+
+import OpenGL
+from OpenGL.GL import *
+
+import numpy, math, sys, os
+import glutils
+
+import glfw
+
+strVS = """
+#version 410 core
+
+layout(location = 0) in vec3 aVert;
+
+uniform mat4 uMVMatrix;
+uniform mat4 uPMatrix;
+uniform float uTheta;
+
+out vec2 vTexCoord;
+
+void main() {
+  // rotational transform
+  mat4 rot =  mat4(
+        vec4(1.0,  0.0,          0.0,         0.0),
+        vec4(0.0,  cos(uTheta),  sin(uTheta), 0.0),
+        vec4(0.0, -sin(uTheta),  cos(uTheta), 0.0),
+	    vec4(0.0,  0.0,          0.0,         1.0)
+	    );
+  // transform vertex
+  gl_Position = uPMatrix * uMVMatrix * rot * vec4(aVert, 1.0); 
+  // set texture coord
+  vTexCoord = aVert.xy + vec2(0.5, 0.5);
+}
+"""
+strFS = """
+#version 410 core
+
+in vec2 vTexCoord;
+
+uniform sampler2D tex2D;
+uniform bool showCircle;
+
+out vec4 fragColor;
+
+void main() {
+  if (showCircle) {
+#if 1
+    // discard fragment outside circle
+    if (distance(vTexCoord, vec2(0.5, 0.5)) > 0.5) {
+      discard;
+    }
+    else {
+      fragColor = texture(tex2D, vTexCoord);
+    }
+#else
+
+   // Answer to home work!
+    
+   #define M_PI 3.1415926535897932384626433832795
+   
+   float r = distance(vTexCoord, vec2(0.5, 0.5));
+   if (sin(16*M_PI*r) < 0.0) {
+      discard;
+   }
+   else {
+      fragColor = texture(tex2D, vTexCoord);
+   }
+#endif
+  }
+  else {
+     fragColor = texture(tex2D, vTexCoord);
+  }
+}
+"""
+
+class Scene:    
+    """ OpenGL 3D scene class"""
+    # initialization
+    def __init__(self):
+        # create shader
+        self.program = glutils.loadShaders(strVS, strFS)
+
+        glUseProgram(self.program)
+
+        self.pMatrixUniform = glGetUniformLocation(self.program, 
+                                                   b'uPMatrix')
+        self.mvMatrixUniform = glGetUniformLocation(self.program, 
+                                                  b'uMVMatrix')
+        # texture 
+        self.tex2D = glGetUniformLocation(self.program, b'tex2D')
+
+        # define triange strip vertices 
+        vertexData = numpy.array(
+            [-0.5, -0.5, 0.0, 
+              0.5, -0.5, 0.0, 
+              -0.5, 0.5, 0.0,
+              0.5, 0.5, 0.0], numpy.float32)
+
+        # set up vertex array object (VAO)
+        self.vao = glGenVertexArrays(1)
+        glBindVertexArray(self.vao)
+        # vertices
+        self.vertexBuffer = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vertexBuffer)
+        # set buffer data 
+        glBufferData(GL_ARRAY_BUFFER, 4*len(vertexData), vertexData, 
+                     GL_STATIC_DRAW)
+        # enable vertex array
+        glEnableVertexAttribArray(0)
+        # set buffer data pointer
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
+        # unbind VAO
+        glBindVertexArray(0)
+
+        # time
+        self.t = 0 
+
+        # texture
+        self.texId = glutils.loadTexture('star.png')
+
+        # show circle?
+        self.showCircle = False
+        
+    # step
+    def step(self):
+        # increment angle
+        self.t = (self.t + 1) % 360
+
+    # render 
+    def render(self, pMatrix, mvMatrix):        
+        # use shader
+        glUseProgram(self.program)
+        
+        # set proj matrix
+        glUniformMatrix4fv(self.pMatrixUniform, 1, GL_FALSE, pMatrix)
+
+        # set modelview matrix
+        glUniformMatrix4fv(self.mvMatrixUniform, 1, GL_FALSE, mvMatrix)
+
+        # set shader angle in radians
+        glUniform1f(glGetUniformLocation(self.program, 'uTheta'), 
+                    math.radians(self.t))
+
+        # show circle?
+        glUniform1i(glGetUniformLocation(self.program, b'showCircle'), 
+                    self.showCircle)
+
+        # enable texture 
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_2D, self.texId)
+        glUniform1i(self.tex2D, 0)
+
+        # bind VAO
+        glBindVertexArray(self.vao)
+        # draw
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
+        # unbind VAO
+        glBindVertexArray(0)
+
+
+class RenderWindow:
+    """GLFW Rendering window class"""
+    def __init__(self):
+
+        # save current working directory
+        cwd = os.getcwd()
+
+        # initialize glfw - this changes cwd
+        glfw.glfwInit()
+        
+        # restore cwd
+        os.chdir(cwd)
+
+        # version hints
+        glfw.glfwWindowHint(glfw.GLFW_CONTEXT_VERSION_MAJOR, 4)
+        glfw.glfwWindowHint(glfw.GLFW_CONTEXT_VERSION_MINOR, 1)
+        glfw.glfwWindowHint(glfw.GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE)
+        glfw.glfwWindowHint(glfw.GLFW_OPENGL_PROFILE, 
+                            glfw.GLFW_OPENGL_CORE_PROFILE)
+    
+        # make a window
+        self.width, self.height = 800, 600
+        self.aspect = self.width/float(self.height)
+        self.win = glfw.glfwCreateWindow(self.width, self.height, 
+                                         b'simpleglfw')
+        # make context current
+        glfw.glfwMakeContextCurrent(self.win)
+        
+        # initialize GL
+        glViewport(0, 0, self.width, self.height)
+        glEnable(GL_DEPTH_TEST)
+        glClearColor(0.5, 0.5, 0.5,1.0)
+
+        # set window callbacks
+        glfw.glfwSetKeyCallback(self.win, self.onKeyboard)
+
+        # create 3D
+        self.scene = Scene()
+
+        # exit flag
+        self.exitNow = False
+
+
+    def onKeyboard(self, win, key, scancode, action, mods):
+        #print 'keyboard: ', win, key, scancode, action, mods
+        if action == glfw.GLFW_PRESS:
+            # ESC to quit
+            if key == glfw.GLFW_KEY_ESCAPE: 
+                self.exitNow = True
+            else:
+                # toggle cut
+                self.scene.showCircle = not self.scene.showCircle 
+        
+    def run(self):
+        # initializer timer
+        glfw.glfwSetTime(0)
+        t = 0.0
+        while not glfw.glfwWindowShouldClose(self.win) and not self.exitNow:
+            # update every x seconds
+            currT = glfw.glfwGetTime()
+            if currT - t > 0.1:
+                # update time
+                t = currT
+                # clear
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+                # set viewport
+                self.width, self.height = glfw.glfwGetFramebufferSize(self.win)
+                self.aspect = self.width/float(self.height)
+                glViewport(0, 0, self.width, self.height)
+                
+                # build projection matrix
+                pMatrix = glutils.perspective(45.0, self.aspect, 0.1, 100.0)
+                
+                mvMatrix = glutils.lookAt([0.0, 0.0, -2.0], [0.0, 0.0, 0.0],
+                                          [0.0, 1.0, 0.0])
+                # render
+                self.scene.render(pMatrix, mvMatrix)
+
+                # step
+                self.scene.step()
+
+                glfw.glfwSwapBuffers(self.win)
+                # Poll for and process events
+                glfw.glfwPollEvents()
+        # end
+        glfw.glfwTerminate()
+
+    def step(self):
+        # step 
+        self.scene.step()
+
+# main() function
+def main():
+    print("Starting simpleglfw. "
+          "Press any key to toggle cut. Press ESC to quit.")
+    rw = RenderWindow()
+    rw.run()
+
+# call main
+if __name__ == '__main__':
+    main()
