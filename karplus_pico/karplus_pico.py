bf720f00a8f5c8d4d008bca9b630fe2e75d1ec25
Mahesh Venkitachalam <mkvenkit@gmail.com>
2022-02-06 12:02:26 +0530

    updated

--- karplus_pico/karplus_pico.py
+++ karplus_pico/karplus_pico.py
@@ -25,7 +25,18 @@ btnNotes = {0: ('C4', 262), 1: ('Eb', 311), 2: ('F', 349), 3: ('G', 391), 4: ('B
 # sample rate
 SR = 16000
 
+def timed_function(f, *args, **kwargs):
+    myname = str(f).split(' ')[1]
+    def new_func(*args, **kwargs):
+        t = time.ticks_us()
+        result = f(*args, **kwargs)
+        delta = time.ticks_diff(time.ticks_us(), t)
+        print('Function {} Time = {:6.3f}ms'.format(myname, delta/1000))
+        return result
+    return new_func
+
 # generate note of given frequency
+# @timed_function 
 def generateNote(freq):
     nSamples = SR
     N = int(SR/freq)
